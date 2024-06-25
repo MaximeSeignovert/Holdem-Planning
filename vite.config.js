c7eb504ceae7af5ce29565d69021ba0ef1707a38
mseignovert <mseignovert@horizontalsoftware.com>
2024-06-25 16:06:46 +0200

    Installation PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,30 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from "vite"
 
+const manifestForPlugin = {
+  registerType:'prompt',
+  includeAssests:['Joker.png'],
+  manifest:{
+    "short_name": "HoldemPlan",
+    "name": "Holdem Planning",
+    "icons": [
+      {
+        "src": "Joker.png",
+        "type": "image/png",
+        "sizes": "256x256"
+      }
+    ],
+    "start_url": ".",
+    "display": "standalone",
+    "theme_color": "#000000",
+    "background_color": "#ffffff"
+  },
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),VitePWA(manifestForPlugin)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
