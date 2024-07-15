e740232e9226dab2d94fdb75a1bea2fd13d716e2
mseignovert <mseignovert@horizontalsoftware.com>
2024-07-15 14:16:00 +0200

    Correction visualisation du ticket

--- src/components/ticket.jsx
+++ src/components/ticket.jsx
@@ -2,12 +2,13 @@ import * as PT from 'prop-types';
 import { Button } from '@/components/ui/button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark, faSquare } from '@fortawesome/free-solid-svg-icons';
+import moment from 'moment/moment';
 
-const formatTime = (time) => {
-    const hours = Math.floor(time / 3600);
-    const minutes = Math.floor((time % 3600) / 60);
-    const seconds = time % 60;
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  const getElapsedTime = (timeStart) => {
+    if(timeStart === null) return '00:00:00';
+    const diffInMilliseconds = moment().diff(timeStart);
+    const duration = moment.duration(diffInMilliseconds);
+    return moment.utc(duration.asMilliseconds()).format('HH:mm:ss');
   };
 
 const Ticket = ({ticket, inProgress, handleCloseItem, time, handleDeleteItem}) => {
@@ -15,7 +16,7 @@ const Ticket = ({ticket, inProgress, handleCloseItem, time, handleDeleteItem}) =
     <div className='flex flex-row items-center  max-w-[100vw] px-2 my-2 h-[50px]'>
         <div className='w-[80%] h-[100%] flex-1 flex flex-row justify-around border items-center bg-white dark:bg-gray-800 backdrop-blur rounded-lg shadow p-1 rounded'>
             <a href={ticket.link} className='font-medium hover:underline' target="_blank" rel="noreferrer"> <p id='ticket-name'>{ticket.name}</p></a>
-            <p>{ticket.startTime} - {ticket.endTime ? ticket.endTime : formatTime(time)}</p>
+            <p>{ticket.startTime} - {ticket.endTime ? ticket.endTime : getElapsedTime(time)}</p>
             
         </div>
         {inProgress && <Button className='ml-2' title='Arrêter le ticket' onClick={handleCloseItem}>
