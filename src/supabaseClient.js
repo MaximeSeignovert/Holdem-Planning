7906857dbf297d1f2dd60846721eb2b43e8a5591
mseignovert <mseignovert@horizontalsoftware.com>
2024-07-25 10:50:34 +0200

    t3

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -5,6 +5,6 @@ const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseKey = import.meta.env.SUPABASE_ANON_KEY;
 
 console.log("Supabase URL:", supabaseUrl);
-console.log("Supabase Anon Key:", supabaseAnonKey);
+console.log("Supabase Anon Key:", supabaseKey);
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
